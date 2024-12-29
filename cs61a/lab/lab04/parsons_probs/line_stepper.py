38ede3fac8e001fb8be6c7cf3c92ae43ac52cbca
wut <macwu1992@hotmail.com>
2024-12-29 17:32:04 +0800

    [lab] finish lab04

---
+++ cs61a/lab/lab04/parsons_probs/line_stepper.py
@@ -0,0 +1,28 @@
+def line_stepper(start, k):
+    """
+    Complete the function line_stepper, which returns the number of ways there are to go from
+    start to 0 on the number line by taking exactly k steps along the number line.
+
+    >>> line_stepper(1, 1)
+    1
+    >>> line_stepper(0, 2)
+    2
+    >>> line_stepper(-3, 3)
+    1
+    >>> line_stepper(3, 5)
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    if start == 0 and k == 0:
+        return 1
+
+    if k == 1:
+        if start == 1 or start  == -1:
+            return 1
+        else:
+            return 0
+
+    if abs(start) > k:
+        return 0
+
+    return line_stepper(start - 1, k - 1) + line_stepper(start + 1, k - 1)
